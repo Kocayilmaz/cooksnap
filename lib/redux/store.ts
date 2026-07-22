e1d5818592edf22f36e740a8ebc4174e057d0e33
Enes Kocayılmaz <115413087+Kocayilmaz@users.noreply.github.com>
2026-07-22 11:29:00 +0200

    Premium API anahtari icin apiKeySlice ekle ve store'a bagla

--- lib/redux/store.ts
+++ lib/redux/store.ts
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import personCountReducer from "./personCountSlice";
 import equipmentReducer from "./equipmentSlice";
+import apiKeyReducer from "./apiKeySlice";
 
 export const makeStore = () =>
   configureStore({
     reducer: {
       personCount: personCountReducer,
       equipment: equipmentReducer,
+      apiKey: apiKeyReducer,
     },
   });
 
